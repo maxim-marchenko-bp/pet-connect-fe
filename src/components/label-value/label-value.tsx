0cbcf1512d8cba5076e936ca7a227e1b8e0dbd38
Maksym Marchenko <mmarchenko@antaeustravel.com>
2026-03-10 14:14:04 +0200

    refactor LabelValue component to use a dedicated interface for props

--- src/components/label-value/label-value.tsx
+++ src/components/label-value/label-value.tsx
@@ -3,13 +3,19 @@
 import { PropsWithChildren, ReactNode, useMemo } from "react";
 import { cn } from "@/lib/class-name/class-name";
 
+interface LabelValueProps {
+  label?: ReactNode;
+  size?: 'small' | 'medium' | 'large';
+  gridCols?: number;
+}
+
 const SIZES_CLASSES = {
   small: 'text-sm',
   medium: 'text-base',
   large: 'text-lg',
 };
 
-export function LabelValue({ label, children, size = 'small', gridCols }: PropsWithChildren<{ label?: ReactNode, size?: 'small' | 'medium' | 'large', gridCols?: number  }>) {
+export function LabelValue({ label, children, size = 'small', gridCols }: PropsWithChildren<LabelValueProps>) {
   const gridColsClass = useMemo(() => gridCols ? `grid grid-cols-${gridCols}` : '', [gridCols]);
   return (
     <div className={cn(SIZES_CLASSES[size], gridColsClass)}>
